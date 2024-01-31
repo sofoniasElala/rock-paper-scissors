75b7b9fde7df07f5978366f5bffb7ded4987da89
sofoniasElala <126926375+sofoniasElala@users.noreply.github.com>
2024-01-30 19:23:04 -0500

    add playRockPaperScissors function

--- rps.js
+++ rps.js
@@ -1,7 +1,26 @@
 function getComputerChoice(){
     randomNum = Math.floor(Math.random() * 3);
 
-    if(randomNum === 1) return "Rock";
-    if(randomNum === 2) return "Paper";
-    return "Scissors";
+    if(randomNum === 1) return "rock";
+    if(randomNum === 2) return "paper";
+    return "scissors";
+}
+
+function playRockPaperScissors(playerSelection, computerSelection){
+
+    if((playerSelection.toLowerCase() === 'rock' && computerSelection === 'rock') || 
+        (playerSelection.toLowerCase() === 'paper' && computerSelection === 'paper') || 
+        (playerSelection.toLowerCase() === 'scissors' && computerSelection === 'scissors')){
+
+              return "Draw!";
+
+        } else if ((playerSelection.toLowerCase() === 'rock' && computerSelection === 'scissors') || 
+        (playerSelection.toLowerCase() === 'paper' && computerSelection === 'rock') || 
+        (playerSelection.toLowerCase() === 'scissors' && computerSelection === 'paper')){
+
+             return `You won! ${playerSelection.toLowerCase()} beats ${computerSelection}.`;
+        } else {
+            return `You lost, ${computerSelection} beats ${playerSelection.toLowerCase()} :(.`;
+        }
+         
 }
