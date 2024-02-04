8dd477a080890c7402b33b642becb46574699f0d
sofoniasElala <126926375+sofoniasElala@users.noreply.github.com>
2024-02-03 19:23:04 -0500

    add JS for the UI elements

--- rps.js
+++ rps.js
@@ -1,3 +1,40 @@
+const buttons = document.querySelectorAll('button');
+const computerHand = document.querySelector('#compHand');
+const playerScore = document.querySelector('#playerScore');
+const computerScore = document.querySelector('#computerScore');
+const winner = document.createElement('div');
+let playerWins = 0;
+let computerWins = 0;
+
+buttons.forEach((button) => {
+    button.addEventListener('click', () => {
+        const computer = getComputerChoice();
+        computerHand.textContent = computer;
+        playRockPaperScissors(button.textContent, computer);
+        updateScoreDisplay();
+    })
+});
+
+
+function updateScoreDisplay(){
+    playerScore.textContent = 'You: ' + playerWins;
+    computerScore.textContent = 'Computer: ' + computerWins;
+
+    if(playerWins === 5) {
+        win('You!');
+    } else if(computerWins === 5){
+       win('Computer :(');
+    }
+}
+
+function win(won){
+    winner.textContent = `Winner: ${won}`;
+        document.body.appendChild(winner);
+        playerWins = 0;
+        computerWins = 0;
+}
+
+
 function getComputerChoice(){
     randomNum = Math.floor(Math.random() * 3);
 
@@ -12,44 +49,15 @@ function playRockPaperScissors(playerSelection, computerSelection){
         (playerSelection.toLowerCase() === 'paper' && computerSelection === 'paper') || 
         (playerSelection.toLowerCase() === 'scissors' && computerSelection === 'scissors')){
 
-              return "Draw!";
+              playerWins += 0;
 
         } else if ((playerSelection.toLowerCase() === 'rock' && computerSelection === 'scissors') || 
         (playerSelection.toLowerCase() === 'paper' && computerSelection === 'rock') || 
         (playerSelection.toLowerCase() === 'scissors' && computerSelection === 'paper')){
 
-             return `You won! ${playerSelection.toLowerCase()} beats ${computerSelection}.`;
+             playerWins++;
         } else {
-            return `You lost, ${computerSelection} beats ${playerSelection.toLowerCase()} :(.`;
+            computerWins++;
         }
          
 }
-
-
-
-function playGame(){
-    let playerScore = 0;
-    let computerScore = 0;
-    let roundWinner = "";
-
-    for(let i = 1; i <= 5; i++){
-     let userInput = prompt(`Round ${i}: What hand do you want to play?`);
-     roundWinner = playRockPaperScissors(userInput, getComputerChoice());
-
-     if(roundWinner.includes("won")){
-        playerScore++;
-       } else if (roundWinner.includes("lost")){
-        computerScore++;
-       } 
-
-       console.log(roundWinner);
-    }
-
-    console.log(`
-    Your Score: ${playerScore}
-    Computer Score: ${computerScore}
-
-    Winner: ${playerScore > computerScore ? "You!" : "Computer"}`);
-}
-
-playGame();
